63de37af40c4f19da6954b0fd76dda8864af180d
Tyler Elliott <tyler.ds.elliott@gmail.com>
2016-12-06 22:31:52 -0500

    v0.11

--- MissileCommand/src/com/tylere/mc/MissileCommandApp.java
+++ MissileCommand/src/com/tylere/mc/MissileCommandApp.java
@@ -8,24 +8,18 @@ import com.almasb.ents.Entity;
 import com.almasb.fxgl.app.ApplicationMode;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.entity.Entities;
-import com.almasb.fxgl.entity.GameEntity;
+import com.almasb.fxgl.entity.EntityView;
+import com.almasb.fxgl.entity.RenderLayer;
 import com.almasb.fxgl.input.Input;
-import com.almasb.fxgl.input.InputMapping;
-import com.almasb.fxgl.input.InputModifier;
-import com.almasb.fxgl.input.Trigger;
 import com.almasb.fxgl.input.UserAction;
-import com.almasb.fxgl.scene.Viewport;
+import com.almasb.fxgl.physics.CollisionHandler;
+import com.almasb.fxgl.physics.PhysicsWorld;
 import com.almasb.fxgl.settings.GameSettings;
-import com.almasb.fxgl.settings.UserProfile;
-import com.almasb.fxgl.time.UpdateEvent;
-import java.util.Map;
 import javafx.animation.PathTransition;
 import javafx.geometry.Point2D;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.QuadCurve;
 import javafx.scene.shape.Rectangle;
@@ -38,73 +32,7 @@ import javafx.util.Duration;
  */
 public class MissileCommandApp extends GameApplication {
 
-    private GameEntity square;
     private Point2D cursor = new Point2D(0, 0);
-
-    @Override
-    protected void initInput() {
-
-    }
-
-    @Override
-    protected void initAssets() {
-
-    }
-
-    @Override
-    protected void initGame() {
-        
-        initMissile();
-        
-        square = Entities.builder()
-                .at(300, 300)
-                .viewFromNode(new Rectangle(20, 20, Color.BLUE))
-                .buildAndAttach(getGameWorld());
-
-        Image image = new Image("/assets/ui/cursors/crosshair.png");
-
-        Point2D dimensions = new Point2D(image.getWidth() / 2, image.getHeight() / 2);
-
-        getGameScene().setCursor("crosshair.png", dimensions);
-    }
-
-    @Override
-    protected void initPhysics() {
-        getPhysicsWorld().setGravity(0, 0);
-    }
-
-    @Override
-    protected void initUI() {
-        Text text = getUIFactory().newText("Missile Command", Color.BLUE, 48);
-        
-        QuadCurve curve = new QuadCurve(-10, 0, getWidth() /2, getHeight(), getWidth() + 100, -20);
-        
-        PathTransition transition = new PathTransition(Duration.seconds(4), curve, text);
-        transition.setOnFinished(e -> {
-            getGameScene().removeUINode(text);
-    });
-        
-    getGameScene().addUINode(text);
-    transition.play();
-    
-    }
-
-    @Override
-    protected void onUpdate(double d) {
-        Input input = getInput();
-        Point2D cursor = input.getMousePositionWorld();
-        System.out.println("Position X: " + cursor.getX() + " Position Y: " + cursor.getY());
-
-        square.removeFromWorld();
-        int size = 20;
-        double x = cursor.getX();
-        double y = cursor.getY();
-
-        square = Entities.builder()
-                .at(cursor.getX() - size / 2, cursor.getY() - size / 2)
-                .viewFromNode(new Rectangle(size, size, Color.BLUE))
-                .buildAndAttach(getGameWorld());
-    }
 
     @Override
     protected void initSettings(GameSettings gs) {
@@ -119,14 +47,121 @@ public class MissileCommandApp extends GameApplication {
         gs.setCloseConfirmation(false);
         gs.setApplicationMode(ApplicationMode.DEVELOPER);
         ///////////// This concludes the window generation section /////////////
+    }
+
+    @Override
+    protected void initInput() {
+        // Mouse click to spawn circle that represents friendly missile
+        Input input = getInput();
+        Point2D cursor = input.getMousePositionWorld();
+
+        double mouseX = cursor.getX();
+        double mouseY = cursor.getY();
+
+        input.addAction(new UserAction("Spawn Anti-Ballastic") {
+            @Override
+            protected void onAction() {
+                Input input = getInput();
+                Point2D cursor = input.getMousePositionWorld();
+
+                double mouseX = cursor.getX();
+                double mouseY = cursor.getY();
+
+                Entity explosion = EntityFactory.newExplosion(mouseX - 15, mouseY - 15);
+                getGameWorld().addEntity(explosion);
+            }
+        }, MouseButton.PRIMARY);
+    }
+
+    @Override
+    protected void initAssets() {
 
     }
-    
-    
-    private void initMissile() {
-        getGameWorld().addEntity(EntityFactory.newMissile(250.0, 250.0));
+
+    @Override
+    protected void initGame() {
+        // Trying to load a background image
+        Pane bg = new Pane();
+
+        Rectangle rectangle = new Rectangle(1000, 1000, Color.BLACK);
+        
+        bg.getChildren().add(rectangle);
+        
+        Entities.builder()
+                .viewFromNode(new EntityView(bg, RenderLayer.BACKGROUND))
+                .buildAndAttach(getGameWorld());
+        
+        
+        // Cursor image
+        Image image = new Image("/assets/ui/cursors/crosshair.png");
+        // You need to use a Point2D to set the cursor image position, to center / 2
+        Point2D dimensions = new Point2D(image.getWidth() / 2, image.getHeight() / 2);
+        // Set the cursor and its proper dimensions
+        getGameScene().setCursor("crosshair.png", dimensions);
+
+        // Cities spawning
+        for (int x = 0; x < 6; x++) {
+            Entity city = EntityFactory.newCity(100 * x + 25, getHeight() - 100);
+            getGameWorld().addEntity(city);
+        }
+
+        ///// Spawning missiles at top of the screen to streak down /////
+        getMasterTimer().runAtInterval(() -> {
+            Entity axisMissile = EntityFactory.newMissile(Math.random() * getWidth() - 64, 0, false);
+
+            getGameWorld().addEntity(axisMissile);
+        }, Duration.millis(250));
     }
-    
+
+    @Override
+    protected void initPhysics() {
+        // Easier to type multiple times
+        PhysicsWorld physicsWorld = getPhysicsWorld();
+
+        // We don't need no stinking gravity
+        physicsWorld.setGravity(0, 0);
+
+        // Setting collisions between explosions and warheads
+        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.EXPLOSION, EntityType.ENEMY_MISSILE) {
+            @Override
+            protected void onCollisionBegin(Entity explosion, Entity missile) {
+                missile.removeFromWorld();
+
+                System.out.println("Hit a warhead");
+            }
+
+        });
+    }
+
+    @Override
+    protected void initUI() {
+        
+        
+        ////////////////////////// Curve code beings /////////////////////////
+        Text text = getUIFactory().newText("Missile Command", Color.BLUE, 48);
+
+        QuadCurve curve = new QuadCurve(-10, 0, getWidth() / 2, getHeight(), getWidth() + 100, -20);
+
+        PathTransition transition = new PathTransition(Duration.seconds(4), curve, text);
+        transition.setOnFinished(e -> {
+            getGameScene().removeUINode(text);
+        });
+
+        getGameScene().addUINode(text);
+        transition.play();
+        /////////////////////////// Curve code ends ///////////////////////////
+    }
+
+    @Override
+    protected void onUpdate(double d) {
+        Input input = getInput();
+        Point2D cursor = input.getMousePositionWorld();
+        System.out.println("Position X: " + cursor.getX() + " Position Y: " + cursor.getY());
+
+        int size = 20;
+        double x = cursor.getX();
+        double y = cursor.getY();
+    }
 
     public static void main(String[] args) {
         launch(args);

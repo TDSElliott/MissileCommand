356e2eab57e55c2a0107cf39b240a6b201265e37
Tyler Elliott <tyler.ds.elliott@gmail.com>
2016-12-11 23:20:47 -0500

    Trying to fix movement code
    Works for one target, have hit wall when it comes to multiple targets. Got a line of (1px x 1px) rectangle objects spawning as a trail but it is too taxing for the system and I have yet to work out the ID system which would allow removal of a line associated with a particular warhead.

--- MissileCommand/src/com/tylere/mc/MissileCommandApp.java
+++ MissileCommand/src/com/tylere/mc/MissileCommandApp.java
@@ -4,33 +4,31 @@
  */
 package com.tylere.mc;
 
-import com.almasb.ents.Entity;
+import com.almasb.ents.Entity; // Entities are the objects that the library can control
 import com.almasb.fxgl.app.ApplicationMode;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.entity.Entities;
-import com.almasb.fxgl.entity.GameEntity;
+import com.almasb.fxgl.entity.EntityView;
+import com.almasb.fxgl.entity.RenderLayer;
+import com.almasb.fxgl.gameplay.GameWorld;
 import com.almasb.fxgl.input.Input;
-import com.almasb.fxgl.input.InputMapping;
-import com.almasb.fxgl.input.InputModifier;
-import com.almasb.fxgl.input.Trigger;
 import com.almasb.fxgl.input.UserAction;
-import com.almasb.fxgl.scene.Viewport;
+import com.almasb.fxgl.physics.CollisionHandler;
+import com.almasb.fxgl.physics.PhysicsWorld;
 import com.almasb.fxgl.settings.GameSettings;
-import com.almasb.fxgl.settings.UserProfile;
-import com.almasb.fxgl.time.UpdateEvent;
-import java.util.Map;
+import javafx.animation.KeyFrame;
 import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
 import javafx.geometry.Point2D;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.QuadCurve;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
+import com.almasb.fxgl.texture.Texture;
 
 /**
  *
@@ -38,12 +36,50 @@ import javafx.util.Duration;
  */
 public class MissileCommandApp extends GameApplication {
 
-    private GameEntity square;
     private Point2D cursor = new Point2D(0, 0);
+    private double mouseX, mouseY;
+
+    @Override
+    protected void initSettings(GameSettings gs) {
+        // The settings code to generate the window and remove FXGL intro for dev
+        gs.setWidth(800);
+        gs.setHeight(700);
+        gs.setTitle("Missile Command");
+        gs.setVersion("0.1");
+        gs.setIntroEnabled(false);
+        gs.setMenuEnabled(false);
+        gs.setProfilingEnabled(false);
+        gs.setCloseConfirmation(false);
+        gs.setApplicationMode(ApplicationMode.DEVELOPER);
+        ///////////// This concludes the window generation section /////////////
+    }
 
     @Override
     protected void initInput() {
+        // Mouse click to spawn circle that represents friendly missile
+        Input input = getInput();
+        Point2D cursor = input.getMousePositionWorld();
 
+        double mouseX = cursor.getX();
+        double mouseY = cursor.getY();
+
+        input.addAction(new UserAction("Spawn Anti-Ballistic") {
+            @Override
+            protected void onAction() {
+                Input input = getInput();
+                Point2D cursor = input.getMousePositionWorld();
+
+                double mouseX = cursor.getX();
+                double mouseY = cursor.getY();
+
+                Entity explosion = EntityFactory.newExplosion(mouseX - 15, mouseY - 15);
+                getGameWorld().addEntity(explosion);
+                Timeline timeline = new Timeline(new KeyFrame(
+                        Duration.millis(250),
+                        ae -> getGameWorld().removeEntity(explosion)));
+                timeline.play();
+            }
+        }, MouseButton.PRIMARY);
     }
 
     @Override
@@ -53,80 +89,112 @@ public class MissileCommandApp extends GameApplication {
 
     @Override
     protected void initGame() {
-        
-        initMissile();
-        
-        square = Entities.builder()
-                .at(300, 300)
-                .viewFromNode(new Rectangle(20, 20, Color.BLUE))
+        // Trying to load a background image
+        Pane bg = new Pane();
+
+        Rectangle rectangle = new Rectangle(1000, 1000, Color.BLACK);
+
+        bg.getChildren().add(rectangle);
+
+        Entities.builder()
+                .viewFromNode(new EntityView(bg, RenderLayer.BACKGROUND))
                 .buildAndAttach(getGameWorld());
 
+        // Cursor image
         Image image = new Image("/assets/ui/cursors/crosshair.png");
 
+        // You need to use a Point2D to set the cursor image position, to center / 2
         Point2D dimensions = new Point2D(image.getWidth() / 2, image.getHeight() / 2);
 
+        // Set the cursor and its proper dimensions
         getGameScene().setCursor("crosshair.png", dimensions);
+
+        // Cities spawning
+        for (int x = 1; x <= 6; x++) {
+            if (x < 4) {
+                Entity city = EntityFactory.newCity(100 * x + 0, getHeight() - 100);
+                getGameWorld().addEntity(city);
+            } else {
+                Entity city = EntityFactory.newCity(100 * x + 50, getHeight() - 100);
+                getGameWorld().addEntity(city);
+            }
+        }
+
+        ///// Spawning missiles at top of the screen to streak down /////
+        getMasterTimer().runAtInterval(() -> {
+            Entity axisMissile = EntityFactory.newMissile(Math.random() * getWidth() - 64, 0, false);
+
+            Point2D direction = new Point2D(10, 10);
+
+            getGameWorld().addEntity(axisMissile);
+        }, Duration.millis(250));
     }
 
     @Override
     protected void initPhysics() {
-        getPhysicsWorld().setGravity(0, 0);
+        // Easier to type multiple times
+        PhysicsWorld physicsWorld = getPhysicsWorld();
+
+        // We don't need no stinking gravity
+        physicsWorld.setGravity(0, 0);
+
+        // Setting collisions between explosions and warheads
+        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.EXPLOSION, EntityType.ENEMY_MISSILE) {
+            @Override
+            protected void onCollisionBegin(Entity explosion, Entity missile) {
+                missile.removeFromWorld();
+            }
+        });
+        
+        // Setting collisions between cities and warheads
+        physicsWorld.addCollisionHandler(new CollisionHandler(EntityType.CITY, EntityType.ENEMY_MISSILE) {
+            @Override
+            protected void onCollisionBegin(Entity city, Entity missile) {
+                missile.removeFromWorld();
+                System.out.println("Your city has been nuked!");
+            }
+        });
     }
 
     @Override
     protected void initUI() {
+        // This code moves the game title across the screen, purely aesthetic
+        ////////////////////////// Curve code beings /////////////////////////
         Text text = getUIFactory().newText("Missile Command", Color.BLUE, 48);
-        
-        QuadCurve curve = new QuadCurve(-10, 0, getWidth() /2, getHeight(), getWidth() + 100, -20);
-        
+
+        QuadCurve curve = new QuadCurve(-10, 0, getWidth() / 2, getHeight(), getWidth() + 100, -20);
+
         PathTransition transition = new PathTransition(Duration.seconds(4), curve, text);
         transition.setOnFinished(e -> {
             getGameScene().removeUINode(text);
-    });
-        
-    getGameScene().addUINode(text);
-    transition.play();
-    
+        });
+
+        getGameScene().addUINode(text);
+        transition.play();
+        /////////////////////////// Curve code ends ///////////////////////////
+
+        Texture texture = getAssetLoader().loadTexture("bg.jpg");
+
+        EntityView bg = new EntityView(texture);
+
+        getGameScene().addGameView(bg);
     }
 
     @Override
     protected void onUpdate(double d) {
         Input input = getInput();
         Point2D cursor = input.getMousePositionWorld();
-        System.out.println("Position X: " + cursor.getX() + " Position Y: " + cursor.getY());
 
-        square.removeFromWorld();
         int size = 20;
         double x = cursor.getX();
         double y = cursor.getY();
-
-        square = Entities.builder()
-                .at(cursor.getX() - size / 2, cursor.getY() - size / 2)
-                .viewFromNode(new Rectangle(size, size, Color.BLUE))
-                .buildAndAttach(getGameWorld());
     }
 
-    @Override
-    protected void initSettings(GameSettings gs) {
-        // The settings code to generate the window and remove FXGL intro for dev
-        gs.setWidth(600);
-        gs.setHeight(600);
-        gs.setTitle("Missile Command");
-        gs.setVersion("0.1");
-        gs.setIntroEnabled(false);
-        gs.setMenuEnabled(false);
-        gs.setProfilingEnabled(false);
-        gs.setCloseConfirmation(false);
-        gs.setApplicationMode(ApplicationMode.DEVELOPER);
-        ///////////// This concludes the window generation section /////////////
+    public GameWorld returnGameWorld() {
+        GameWorld gw = getGameWorld();
 
+        return gw;
     }
-    
-    
-    private void initMissile() {
-        getGameWorld().addEntity(EntityFactory.newMissile(250.0, 250.0));
-    }
-    
 
     public static void main(String[] args) {
         launch(args);
--- MissileCommand/src/com/tylere/mc/control/MissileControl.java
+++ MissileCommand/src/com/tylere/mc/control/MissileControl.java
@@ -1,33 +1,88 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This class controls the missiles I spawn in, mostly movement controls
+ * The FXGL library has a method for adding controls, this is what that refers to
+ * It connects a class formatted as an  Abstract Control to the Entity (FXGL's object)
  */
 package com.tylere.mc.control;
 
 import com.almasb.ents.AbstractControl;
 import com.almasb.ents.Entity;
-import com.almasb.ents.component.Required;
+import com.almasb.fxgl.app.FXGL;
 import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.entity.component.IDComponent;
 import com.almasb.fxgl.entity.component.PositionComponent;
-import com.almasb.fxgl.physics.PhysicsComponent;
+import com.tylere.mc.EntityFactory;
+import javafx.geometry.Point2D;
 
 /**
  *
- * @author Tyler
+ * @author Tyler D.S. Elliott
  */
 public class MissileControl extends AbstractControl {
 
-    private PhysicsComponent missile;
-    
-     @Override
-    public void onAdded(Entity entity) {
-        missile = entity.getComponentUnsafe(PhysicsComponent.class);
+    private Point2D velocity;
+    private static int count = 1;
+    private int id, x, y, rand;
+    private IDComponent testID;
+
+    public MissileControl() {
+        velocity = new Point2D(0, 0);
+        id = count++;
+        IDComponent testID = new IDComponent("trail", 5);
+        
+        int rand = (int) (Math.random() * 9);
+        switch (rand) {
+            case 0: x = 40; y = 600;
+            case 1: x = 156; y = 600;
+            case 2: x = 243; y = 600;
+            case 3: x = 330; y = 600;
+            case 4: x = 400; y = 600;
+            case 5: x = 471; y = 600;
+            case 6: x = 558; y = 600;
+            case 7: x = 645; y = 600;
+            case 8: x = 754; y = 600;
+        }
+        System.out.println("rand generated: " + rand);
     }
-    
+
     @Override
     public void onUpdate(Entity entity, double d) {
         
-    }
+        
+        System.out.println(rand + " " + x + " " + y);
+        
+        
+        
+        Point2D baseDirection = new Point2D(x, y)
+                .subtract(Entities.getPosition(entity).getValue())
+                .normalize()
+                .multiply(1000);
 
+        velocity = velocity.add(baseDirection)
+                .multiply(0.65);
+
+        Entities.getPosition(entity).translate(velocity.multiply(d * 0.1f));
+
+        // rotate
+        if (velocity.getX() != 0 && velocity.getY() != 0) {
+            Entities.getRotation(entity).rotateToVector(velocity);
+        }
+        
+//        PositionComponent location = entity.getComponentUnsafe(PositionComponent.class);
+//        
+//        Entity trail = EntityFactory.newTrail(location.getX(), location.getY(), id);
+//        
+//        FXGL.getApp().getGameWorld().addEntity(trail);
+//        System.out.println(testID.getFullID());
+//        
+//        if (trail.getComponent(IDComponent.class).equals(testID)) {
+//            System.out.println("Hooah!");
+//        }
+
+    }
+   
+    
+    public int getID() {
+        return id;
+    }
 }
